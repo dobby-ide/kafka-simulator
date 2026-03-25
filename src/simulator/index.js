68c1063c3fb723e7e1e8e0df729f8089e79cea95
Fabio Privitera <fabio_privitera@yahoo.it>
2026-03-25 10:24:58 +0200

    kafka producer is the main responsible for storing the events

--- src/simulator/index.js
+++ src/simulator/index.js
@@ -3,6 +3,8 @@ const { updateState } = require('./stateUpdater');
 const { generateEvents } = require('./eventGenerator');
 const { emitEvents } = require('./emitter');
 
+const { connectProducer, sendEvents } = require('../producer/producer');
+
 const devices = [
   createDevice('watch-001'),
   createDevice('watch-002'),
@@ -10,13 +12,20 @@ const devices = [
 ];
 
 let tick = 0;
+async function startSimulator() {
+  await connectProducer();
 
-setInterval(() => {
-  tick++;
+  setInterval(async () => {
+    tick++;
 
-  devices.forEach((device) => {
-    const stateEvents = updateState(device, tick);
-    const generatedEvents = generateEvents(device, tick);
-    emitEvents([...stateEvents, ...generatedEvents]);
-  });
-}, 1000);
+    for (const device of devices) {
+      const stateEvents = updateState(device, tick);
+      const generatedEvents = generateEvents(device, tick);
+      const allEvents = [...stateEvents, ...generatedEvents];
+
+      await sendEvents(allEvents);
+    }
+  }, 1000);
+}
+
+startSimulator().catch(console.error);
